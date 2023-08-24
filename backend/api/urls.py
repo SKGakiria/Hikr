fdaf80e4b49da4ada8a0d349e9c2a4941df50759
Evance Odoyo <odoyoevanc@gmail.com>
2023-08-24 15:05:40 +0300

    Add authentication urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from users.views import UserListCreateView, UserRetrieveUpdateDeleteView
+from users.views import UserListView, UserRetrieveUpdateDeleteView, UserRegisterView, LoginView
 from groups.views import GroupListCreateView, GroupRetrieveUpdateDeleteView, GroupMembershipListCreateView, GroupMembershipDeleteView
 from events.views import EventListCreateView, EventRetrieveUpdateDeleteView, EventAttendanceListCreateView, EventAttendanceDeleteView
 from .views import api_root
-
+from knox import views as knox_views
 
 urlpatterns = [
     path('', api_root, name='api-root'),
@@ -14,7 +14,11 @@ urlpatterns = [
     path("events/", EventListCreateView.as_view(), name="event-list"),
     path("events/<int:pk>/", EventRetrieveUpdateDeleteView.as_view(), name="event-detail"),
     path("events/<int:pk>/participants/", EventAttendanceListCreateView.as_view(), name="api-event-participants-list"),
-    path("events/<int:pk>/participants/<int:participant_pk>/", EventAttendanceDeleteView.as_view(), name="event-participants-detail"),
-    path("users/", UserListCreateView.as_view(), name="user-list"),
+    path("events/<int:pk>/participants/<int:participant_pk>/", EventAttendanceDeleteView.as_view(), name="event-participants-delete"),
+    path("users/", UserListView.as_view(), name="user-list"),
     path("users/<int:pk>/", UserRetrieveUpdateDeleteView.as_view(), name="user-detail"),
+    path("users/register/", UserRegisterView.as_view(), name="user-register"),
+    path("users/login/", LoginView.as_view(), name="knox_login"),
+    path("users/logout/", knox_views.LogoutView.as_view(), name="knox_logout"),
+    path("users/logoutall/", knox_views.LogoutAllView.as_view(), name="knox_logoutall"),
 ]
