47e8e8a753da5dd83c6ce42fed367f085900e8cb
Evance Odoyo <odoyoevanc@gmail.com>
2023-08-24 15:06:46 +0300

    Remove unused methods

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-# from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django_resized import ResizedImageField
 
