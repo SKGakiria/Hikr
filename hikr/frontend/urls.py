f3f9fc6e168c7af1e5f071430eea55b1841e3ed9
Evance Odoyo <odoyoevanc@gmail.com>
2023-08-31 18:12:24 +0300

    Add routes and views to render templates

--- hikr/frontend/urls.py
+++ hikr/frontend/urls.py
@@ -2,4 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('groups/', views.groups_list, name='groups'),
+    path('events/', views.events_list, name='events'),
+    path('group/', views.group, name='group'),
+    path('event/', views.event, name='event'),
+    path('sign-up/', views.sign_up, name='sign-up'),
+    path('login/', views.login, name='login'),
 ]
--- hikr/frontend/views.py
+++ hikr/frontend/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    Render the homepage/landing page of the application.
+    """
+    return render(request, 'index.html')
+
+
+def groups_list(request):
+    """
+    Render list of groups.
+    """
+    return render(request, 'groups.html')
+
+
+def events_list(request):
+    """
+    Render list of events.
+    """
+    return render(request, 'events.html')
+
+def group(request):
+    """
+    Render group page.
+    """
+    return render(request, 'group.html')
+
+def event(request):
+    """
+    Render event page.
+    """
+    return render(request, 'event.html')
+
+
+def sign_up(request):
+    """
+    Render sign up page.
+    """
+    return render(request, 'sign-up.html')
+
+
+def login(request):
+    """
+    Render login page.
+    """
+    return render(request, 'login.html')
